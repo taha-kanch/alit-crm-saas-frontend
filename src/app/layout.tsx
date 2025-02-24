c385329ae4594ca6d1ff6b66d46c24ad287c9825
Taha kanchwala <tahakanchwala@alittech.com>
2025-02-24 19:32:45 +0530

    created a new fontend for alit crm

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { SidebarProvider } from "@/context/SidebarContext";
+import { ThemeProvider } from "@/context/ThemeContext";
+import { ToastContainer } from "react-toastify";
+
+const outfit = Outfit({
+  variable: "--font-outfit-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.variable} dark:bg-gray-900`}>
+        <ThemeProvider>
+          <SidebarProvider>{children}</SidebarProvider>
+          <ToastContainer/>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/utils/constants.tsx
@@ -0,0 +1,35 @@
+
+export interface SignupValues {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: String;
+    // confirmPassword: string;
+}
+
+export interface LoginValues {
+    username: string;
+    password: String;
+}
+
+export interface AuthHeader {
+    method: string,
+    headers: {
+        'Authorization'?: string,
+        'Content-Type': string,
+    },
+    body?: string,
+}
+
+export interface SubscriptionPlan {
+    active: boolean,
+    description: string,
+    features: [],
+    id: number,
+    price: number,
+    pricePerMonth: number,
+    subscriptionType: {
+        typeName: string
+    },
+    typeID: number
+}
